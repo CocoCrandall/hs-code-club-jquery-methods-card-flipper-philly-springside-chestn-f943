05635e95ac9dc5422f8fe3f40c60420730a375f5
CocoCrandall <acrandall@sch.org>
2016-03-11 21:09:11 +0000

    nitrous-backup at Fri Mar 11 21:09:11 UTC 2016

--- js/card.js
+++ js/card.js
@@ -6,6 +6,15 @@ $( function(){
 
 
 	// code click event for card-two here
+$( function(){
 
+  $( "#card-two").click(function(){
+    //Use toggleClass method here
+  });
   // code click event for card-three here
+  $( function(){
+
+  $( "#card-three").click(function(){
+    //Use toggleClass method here
+  });
  })
